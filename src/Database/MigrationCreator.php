c2dc6646351b92be67e7bc011368ba6c8b64ffd8
Mior Muhammad Zaki <crynobone@gmail.com>
2020-01-02 10:20:55 +0800

    Return exception if path doesn't exists.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Database/MigrationCreator.php
+++ src/Database/MigrationCreator.php
@@ -2,6 +2,7 @@
 
 namespace Laravie\Canvas\Database;
 
+use InvalidArgumentException;
 use Laravie\Canvas\Presets\Preset;
 
 class MigrationCreator extends \Illuminate\Database\Migrations\MigrationCreator
@@ -23,11 +24,32 @@ class MigrationCreator extends \Illuminate\Database\Migrations\MigrationCreator
     }
 
     /**
+     * Create a new migration at the given path.
+     *
+     * @param  string  $name
+     * @param  string  $path
+     * @param  string|null  $table
+     * @param  bool  $create
+     *
+     * @throws \Exception
+     *
+     * @return string
+     */
+    public function create($name, $path, $table = null, $create = false)
+    {
+        if (! $this->files->isDirectory($path)) {
+            throw new InvalidArgumentException("Path {$path} doesn't exists.");
+        }
+
+        return parent::create($name, $path, $table, $create);
+    }
+
+    /**
      * Get the path to the stubs.
      *
      * @return string
      */
-    public function stubPath(): string
+    public function stubPath()
     {
         return __DIR__.'/../../storage/database/migrations';
     }
