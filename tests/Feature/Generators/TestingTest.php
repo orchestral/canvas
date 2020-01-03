2429b2f18e72d5f7ca182a76f370f660971a38c6
Mior Muhammad Zaki <crynobone@gmail.com>
2020-01-03 18:32:07 +0800

    Testing on package preset.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/Generators/TestingTest.php
+++ tests/Feature/Generators/TestingTest.php
@@ -2,6 +2,8 @@
 
 namespace Orchestra\Canvas\Tests\Feature\Generators;
 
+use Orchestra\Canvas\Presets\Package;
+
 class TestingTest extends TestCase
 {
     protected $files = [
@@ -17,6 +19,8 @@ class TestingTest extends TestCase
 
         $this->assertFileContains([
             'namespace Tests\Feature;',
+            'use Illuminate\Foundation\Testing\RefreshDatabase;',
+            'use Illuminate\Foundation\Testing\WithFaker;',
             'use Tests\TestCase;',
             'class FooTest extends TestCase',
         ], 'tests/Feature/FooTest.php');
@@ -33,5 +37,23 @@ class TestingTest extends TestCase
             'use PHPUnit\Framework\TestCase;',
             'class FooTest extends TestCase',
         ], 'tests/Unit/FooTest.php');
+    }
+
+    /** @test */
+    public function it_can_generate_feature_test_file_on_package_preset()
+    {
+        $this->instance('orchestra.canvas', new Package(
+            ['namespace' => 'Foo', 'testing' => ['namespace' => 'Foo\Tests']], $this->app->basePath(), $this->filesystem
+        ));
+
+        $this->artisan('make:test', ['name' => 'FooTest'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace Foo\Tests\Feature;',
+            'use Illuminate\Foundation\Testing\RefreshDatabase;',
+            'use Orchestra\Testbench\TestCase;',
+            'class FooTest extends TestCase',
+        ], 'tests/Feature/FooTest.php');
     }
 }
