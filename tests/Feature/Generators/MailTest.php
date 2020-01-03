45c05a3f841fc42b4a29fa6fb1a2a456cfd79f56
Mior Muhammad Zaki <crynobone@gmail.com>
2020-01-03 17:56:41 +0800

    Test make:mail generator. Closes #2
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Feature/Generators/MailTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Orchestra\Canvas\Tests\Feature\Generators;
+
+class MailTest extends TestCase
+{
+    protected $files = [
+        'app/Mail/FooMail.php',
+        'resources/views/foo-mail.blade.php',
+    ];
+
+    /** @test */
+    public function it_can_generate_mail_file()
+    {
+        $this->artisan('make:mail', ['name' => 'FooMail'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Mail;',
+            'use Illuminate\Mail\Mailable;',
+            'class FooMail extends Mailable',
+        ], 'app/Mail/FooMail.php');
+    }
+
+    /** @test */
+    public function it_can_generate_mail_with_markdown_options_file()
+    {
+        $this->artisan('make:mail', ['name' => 'FooMail', '--markdown' => 'foo-mail'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Mail;',
+            'use Illuminate\Mail\Mailable;',
+            'class FooMail extends Mailable',
+        ], 'app/Mail/FooMail.php');
+
+        $this->assertFileContains([
+            "@component('mail::message')",
+        ], 'resources/views/foo-mail.blade.php');
+    }
+}
