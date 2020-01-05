2aa7d14b74a91c80a05a1bc0215629dca316a62c
Mior Muhammad Zaki <crynobone@gmail.com>
2020-01-05 23:22:04 +0800

    Update CS.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Processors/GeneratesControllerCode.php
+++ src/Processors/GeneratesControllerCode.php
@@ -58,7 +58,6 @@ class GeneratesControllerCode extends GeneratesCode
     {
         $modelClass = $this->parseModel($this->options['model']);
 
-
         if (! \class_exists($modelClass) && \method_exists($this->listener, 'createModel')) {
             $this->listener->createModel($modelClass);
         }
