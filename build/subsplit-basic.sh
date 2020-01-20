b57dc1ad39f4c1d2324a55267f95dbe1181bb054
Mior Muhammad Zaki <crynobone@gmail.com>
2020-01-20 19:30:49 +0800

    Remove build folder.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -d .subsplit ]; then
-    git subsplit update
-else
-    git subsplit init git@github.com:orchestral/canvas.git
-fi
-
-git subsplit publish --heads="4.x" --no-tags src/Core:git@github.com:orchestral/canvas-core.git
