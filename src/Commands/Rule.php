b9d0d2439e2cac81c95b20798d977032256e4858
Mior Muhammad Zaki <crynobone@gmail.com>
2022-06-22 11:26:54 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Commands/Rule.php
+++ src/Commands/Rule.php
@@ -59,6 +59,10 @@ class Rule extends Generator
      */
     public function getStubFileName(): string
     {
+        if ($this->option('invokable')) {
+            return $this->option('implicit') ? 'rule.invokable.implicit.stub' : 'rule.invokable.stub';
+        }
+
         return 'rule.stub';
     }
 
@@ -79,6 +83,7 @@ class Rule extends Generator
     {
         return [
             'implicit' => $this->option('implicit') ?? false,
+            'invokable' => $this->option('invokable') ?? false,
         ];
     }
 
@@ -91,6 +96,7 @@ class Rule extends Generator
     {
         return [
             ['implicit', 'i', InputOption::VALUE_NONE, 'Generate an implicit rule.'],
+            ['invokable', null, InputOption::VALUE_NONE, 'Generate a single method, invokable rule class.'],
         ];
     }
 }
