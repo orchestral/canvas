5a19bc43ec539f36114a82daab888a7a176d3d6e
Mior Muhammad Zaki <crynobone@gmail.com>
2022-10-12 08:53:17 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/Generators/NotificationTest.php
+++ tests/Feature/Generators/NotificationTest.php
@@ -36,7 +36,7 @@ class NotificationTest extends TestCase
         ], 'app/Notifications/FooNotification.php');
 
         $this->assertFileContains([
-            "<x-mail::message>",
+            '<x-mail::message>',
         ], 'resources/views/foo-notification.blade.php');
     }
 }
