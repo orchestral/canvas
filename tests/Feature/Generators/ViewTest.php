d522d304f44c9a6f77d8d5d2d4cdc9ac1ac911c9
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-12 21:25:52 +0800

    Add `make:view` command (#23)
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Feature/Generators/ViewTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Orchestra\Canvas\Tests\Feature\Generators;
+
+class ViewTest extends TestCase
+{
+    protected $files = [
+        'resources/views/foo.blade.php',
+        'tests/Feature/View/FooTest.php',
+    ];
+
+    /** @test */
+    public function it_can_generate_feature_view_file()
+    {
+        $this->artisan('make:view', ['name' => 'foo'])
+            ->assertExitCode(0);
+
+        $this->assertFilenameExists('resources/views/foo.blade.php');
+        $this->assertFilenameNotExists('tests/Feature/View/FooTest.php');
+    }
+
+    /** @test */
+    public function it_can_generate_feature_view_file_with_tests()
+    {
+        $this->artisan('make:view', ['name' => 'foo', '--test' => true])
+            ->assertExitCode(0);
+
+        $this->assertFilenameExists('resources/views/foo.blade.php');
+        $this->assertFilenameExists('tests/Feature/View/FooTest.php');
+    }
+}
