10f49b2ae5df5a1f2964b474f9be26a5149113a5
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-13 14:03:14 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Commands/ConsoleGenerator.php
+++ src/Commands/ConsoleGenerator.php
@@ -2,9 +2,7 @@
 
 namespace Orchestra\Canvas\Commands;
 
-use Illuminate\Support\Str;
 use Symfony\Component\Console\Attribute\AsCommand;
-use Symfony\Component\Console\Input\InputOption;
 
 #[AsCommand(name: 'make:generator', description: 'Create a new generator command')]
 class ConsoleGenerator extends \Orchestra\Canvas\Core\Commands\Generators\ConsoleGenerator
