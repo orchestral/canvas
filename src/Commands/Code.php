8bed1a4961e1ac26eb17010b96c1cdb6cc54027c
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-13 13:59:41 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Commands/Code.php
+++ src/Commands/Code.php
@@ -5,28 +5,7 @@ namespace Orchestra\Canvas\Commands;
 use Symfony\Component\Console\Attribute\AsCommand;
 
 #[AsCommand(name: 'make:class', description: 'Create a new class')]
-class Code extends Generator
+class Code extends \Orchestra\Canvas\Core\Commands\Generators\Code
 {
-    /**
-     * The type of class being generated.
-     *
-     * @var string
-     */
-    protected string $type = 'Class';
-
-    /**
-     * Get the stub file for the generator.
-     */
-    public function getStubFile(): string
-    {
-        return $this->getStubFileName();
-    }
-
-    /**
-     * Get the stub file name for the generator.
-     */
-    public function getStubFileName(): string
-    {
-        return __DIR__.'/../../storage/canvas/code.stub';
-    }
+    //
 }
