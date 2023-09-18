0faf8e20ca9cacd43ce92559d7c8e4d44b87cf99
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-18 20:30:38 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Canvas.php
+++ src/Canvas.php
@@ -8,31 +8,6 @@ use Illuminate\Support\Collection;
 class Canvas
 {
     /**
-     * Assume the preset from environment.
-     */
-    public static function presetFromEnvironment(string $basePath): string
-    {
-        /** detect `testbench.yaml` */
-        $testbenchYaml = Collection::make([
-            'testbench.yaml',
-            'testbench.yaml.example',
-            'testbench.yaml.dist',
-        ])->filter(fn ($filename) => file_exists($basePath.DIRECTORY_SEPARATOR.$filename))
-            ->first();
-
-        if (! \is_null($testbenchYaml)) {
-            return 'package';
-        }
-
-        return Collection::make([
-            file_exists($basePath.DIRECTORY_SEPARATOR.'artisan'),
-            file_exists($basePath.DIRECTORY_SEPARATOR.'bootstrap'.DIRECTORY_SEPARATOR.'app.php'),
-            is_dir($basePath.DIRECTORY_SEPARATOR.'bootstrap'.DIRECTORY_SEPARATOR.'cache'),
-        ])->reject(fn ($condition) => $condition === true)
-            ->isEmpty() ? 'laravel' : 'package';
-    }
-
-    /**
      * Make Preset from configuration.
      *
      * @param  array<string, mixed>  $config
