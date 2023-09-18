39d4745e46adb94afa18facecb8cb988e5d8c21b
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-18 20:16:58 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Feature/Console/ResourceMakeCommandTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Orchestra\Canvas\Tests\Feature\Generators;
+
+use Orchestra\Canvas\Tests\Feature\TestCase;
+
+class ResourceMakeCommandTest extends TestCase
+{
+    protected $files = [
+        'app/Http/Resources/FooResource.php',
+    ];
+
+    /** @test */
+    public function it_can_generate_resource_file()
+    {
+        $this->artisan('make:resource', ['name' => 'FooResource', '--preset' => 'canvas'])
+            ->assertSuccessful();
+
+        $this->assertFileContains([
+            'namespace App\Http\Resources;',
+            'use Illuminate\Http\Resources\Json\JsonResource;',
+            'class FooResource extends JsonResource',
+            'public function toArray($request)',
+        ], 'app/Http/Resources/FooResource.php');
+    }
+
+
+    /** @test */
+    public function it_can_generate_resource_collection_file()
+    {
+        $this->artisan('make:resource', ['name' => 'FooResource', '--collection' => true, '--preset' => 'canvas'])
+            ->assertSuccessful();
+
+        $this->assertFileContains([
+            'namespace App\Http\Resources;',
+            'use Illuminate\Http\Resources\Json\ResourceCollection;',
+            'class FooResource extends ResourceCollection',
+            'public function toArray($request)',
+        ], 'app/Http/Resources/FooResource.php');
+    }
+}
