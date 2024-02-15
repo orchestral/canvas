2d783bae69fd2177633a0f1e7242b3d28f518552
Mior Muhammad Zaki <crynobone@gmail.com>
2024-02-16 07:23:52 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/InterfaceMakeCommand.php
+++ src/Console/InterfaceMakeCommand.php
@@ -5,6 +5,7 @@ namespace Orchestra\Canvas\Console;
 use Illuminate\Filesystem\Filesystem;
 use Orchestra\Canvas\Core\Concerns\CodeGenerator;
 use Orchestra\Canvas\Core\Concerns\UsesGeneratorOverrides;
+use Symfony\Component\Console\Attribute\AsCommand;
 
 /**
  * @see https://github.com/laravel/framework/blob/master/src/Illuminate/Foundation/Console/InterfaceMakeCommand.php
